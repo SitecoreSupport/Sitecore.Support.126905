e0a39bfb62bc059442bed8e67ed2474e5e4b94f2
DK\mao <mao@sitecore.net>
2017-05-11 16:18:57 +0300

    Fix css message.

---
+++ src/Sitecore.Support.126905/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.126905")]
+[assembly: AssemblyProduct("Sitecore.Support.126905")]
+[assembly: ComVisible(false)]
